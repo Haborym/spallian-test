9f54429a0ca640514de6c9df8da89f7f540be9b6
Arthur Vadrot <krafteck666@gmail.com>
2024-05-04 12:01:27 +0200

    fix: 🐛 Added the key to the HeaderItem

--- src/components/Header-list.tsx
+++ src/components/Header-list.tsx
@@ -8,7 +8,7 @@ export const HeaderList = () => {
     <div className="header-list">
       {navigation &&
         navigation.map((el) => {
-          return <HeaderItem name={el.name} link={el.link} />;
+          return <HeaderItem name={el.name} link={el.link} key={el.link} />;
         })}
     </div>
   );
