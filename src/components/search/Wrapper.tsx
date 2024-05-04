eb158f5373e67f6eecb2bfce9bb2e026217556a6
Arthur Vadrot <krafteck666@gmail.com>
2024-05-04 22:14:10 +0200

    feat: ✨ Added the loader for the search so the user has an idea of what is going on

--- src/components/search/Wrapper.tsx
+++ src/components/search/Wrapper.tsx
@@ -2,18 +2,22 @@ import React, { useState } from "react";
 import { SearchBar } from "./SearchBar";
 import { SearchResult } from "./Search-result";
 import { Books } from "../../utils/types";
+import { BooksLoader } from "../loaders/Books-loader";
 
 export const SearchWrapper = () => {
   const [searchResult, setSearchResult] = useState<Books>();
   const [searchInput, setSearchInput] = useState("");
+  const [searchStatus, setSearchStatus] = useState(false);
 
   const search = async () => {
+    setSearchStatus(true);
     const response = await fetch(
       `http://gutendex.com/books?search=${searchInput}`
     );
 
     if (response) {
       setSearchResult(await response.json());
+      setSearchStatus(false);
     }
   };
 
@@ -24,7 +28,10 @@ export const SearchWrapper = () => {
         setSearchInput={setSearchInput}
         search={search}
       />
-      {searchResult && <SearchResult data={searchResult} input={searchInput} />}
+      {!searchStatus && searchResult && (
+        <SearchResult data={searchResult} input={searchInput} />
+      )}
+      {searchStatus && <BooksLoader />}
     </>
   );
 };
